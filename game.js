ea385eed0644798dbe2fe7a0c51924269e5e1899
Beder Danilo Casa Condori <108836354+BederCC@users.noreply.github.com>
2024-07-21 05:42:17 -0500

    Update game.js

--- game.js
+++ game.js
@@ -1,13 +1,32 @@
-const canvas = document.querySelector('#game');
-const game = canvas.getContext('2d');
+const canvas = document.querySelector("#game");
+const game = canvas.getContext("2d");
+let canvasSize;
+let elementsSize;
 
-window.addEventListener('load', startGame);
+window.addEventListener("load", setCanvasSize);
+window.addEventListener("resize", setCanvasSize);
+
+function setCanvasSize() {
+    if (window.innerHeight > window.innerWidth) {
+        canvasSize = window.innerWidth * 0.8;
+    } else {
+        canvasSize = window.innerHeight * 0.8;
+    }
+    canvas.setAttribute("width", canvasSize);
+    canvas.setAttribute("height", canvasSize);
+
+    elementsSize = (canvasSize / 10);
+    
+    startGame();
+}
 
 function startGame() {
-    game.fillStyle = 'black';
-    game.fillRect(0, 0, canvas.width, canvas.height);
-    game.fillStyle = 'white';
-    game.font = '30px Arial';
-    game.fillText('Hello World', 100, 100);
+    console.log({canvasSize, elementsSize });
 
+    game.font = elementsSize + 'px Verdana';
+    game.textAlign = "end";
+
+    for (let i = 1; i <= 10; i++) {
+        game.fillText(emojis['X'],elementsSize+9, elementsSize*i);
+    }
 }
